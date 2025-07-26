48f3582f193823a531a4bb153d9e38de641ae043
RAITILAK <raitilak1212@gmail.com>
2025-07-26 15:05:19 +0530

    number-challange

---
+++ 01-variables-data-types/12-number-challange/script.js
@@ -0,0 +1,10 @@
+const x = Math.floor(Math.random() * 100 + 1);
+const y = Math.floor(Math.random() * 50 + 1);
+
+const sum = x + y;
+const sumOutput = `The sum of ${x} and ${y} is ${sum}`;
+const difference = x - y;
+const product = x * y;
+const quotient = x / y;
+const remainder = x % y;
+console.log(sumOutput);
