790adab55e20ada5732083b9292241fda21d4e50
RAITILAK <raitilak1212@gmail.com>
2025-07-26 14:28:54 +0530

    math-object

---
+++ 01-variables-data-types/11-math-object/script.js
@@ -0,0 +1,23 @@
+let x;
+
+x = Math.sqrt(7);
+// Square root of 7
+x = Math.pow(2, 3); // 2 raised to the power of
+// 3
+x = Math.abs(-7);
+// Absolute value of -7
+x = Math.ceil(7.4); // Round up 7.4
+x = Math.floor(7.4); // Round down 7.4
+
+x = Math.round(7.4);
+// Round 7.4 to the nearest integer
+x = Math.round(7.5); // Round 7.5 to the nearest integer
+x = Math.pow(2, 3); // 2 raised to the power of 3
+
+x = Math.min(1, 2, 3, 4, 5); // Minimum value
+x = Math.max(1, 2, 3, 4, 5); // Maximum value
+x = Math.random(); // Random number between 0 and 1
+
+let y = Math.random() * 10; // Random number between 0 and 10
+y = Math.floor(y); // Round down to get an integer
+console.log(y);
